0a2aa0ca1444fd0ec51e9b8408466df7b47d8965
Ranidu Harshana <68012743+ranidu-harshana@users.noreply.github.com>
2022-05-10 12:14:37 +0530

    developer details

--- resources/views/components/admin/admin-top-nav.blade.php
+++ resources/views/components/admin/admin-top-nav.blade.php
@@ -7,6 +7,29 @@
     <a id="toggle_btn" href="javascript:void(0);"><i class="fa fa-bars"></i></a>
     <a id="mobile_btn" class="mobile_btn float-left" href="#sidebar"><i class="fa fa-bars"></i></a>
     <ul class="nav user-menu float-right">
+        <li class="nav-item dropdown d-none d-sm-block">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown"><i class="fas fa-question-circle"></i> </a>
+            <div class="dropdown-menu notifications">
+                <div class="topnav-dropdown-header">
+                    <span>Developer Details</span>
+                </div>
+                <div class="drop-scroll">
+                    <ul class="notification-list">
+                        <li class="notification-message">
+                            <a>
+                                <div class="media">
+                                    <div class="media-body">
+                                        <p class="noti-details"><span class="noti-title">Developed By: </span> Ranidu Harshana</p>
+                                        <p class="noti-details"><span class="noti-title">Mobile: </span> 077 600 6495</p>
+                                        <p class="noti-details"><span class="noti-title">Email: </span> harshanaranidum@gmail.com</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </li>
         <li class="nav-item dropdown has-arrow">
             <a href="#" class="dropdown-toggle nav-link user-link" data-toggle="dropdown">
                 <span class="user-img">
